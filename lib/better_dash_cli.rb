84e2aec2bbee7f35c23d4147989a647263f71703
Zac Wasielewski <zac@wasielewski.org>
2015-01-20 20:12:03 -0500

    Refactoring

--- lib/better_dash_cli.rb
+++ lib/better_dash_cli.rb
@@ -1,26 +1,54 @@
+require "ostruct"
 require "better_dash_cli/version"
 
 module BetterDashCli
-
-  class CommandBuilder
-
-    def self.command_from_options opts
-      case opts.length
+  
+  class ArgumentParser
+    
+    def self.parse(args)
+      
+      options = OpenStruct.new
+      options.language = nil
+      options.keyword  = nil
+      
+      case args.length
       when 0
         raise ArgumentError, "Wrong number of arguments (0 for 1..2)"
       when 1
-        search = opts[0]
-        cmd    = "dash://#{search}"  
+        options.keyword  = args[0]
       when 2
-        lang   = opts[0]
-        search = opts[1]
-        cmd    = "dash://#{lang}:#{search}"
+        options.language = args[0]
+        options.keyword  = args[1]
       else
-        raise ArgumentError, "Wrong number of arguments (#{opts.length} for 1..2)"
+        raise ArgumentError, "Wrong number of arguments (#{args.length} for 1..2)"
       end
-      cmd
+      
+      options
     end
-
+    
+  end
+  
+  class Dash
+    
+    @language = nil
+    @keyword  = nil
+    
+    def initialize(args)
+      options = ArgumentParser.parse(args)
+      @language = options.language
+      @keyword  = options.keyword
+    end
+    
+    def command
+      raise Exception if @keyword.nil?
+      if !@language.nil?
+        command = "dash://#{@language}:#{@keyword}"
+      else
+        command = "dash://#{@keyword}"
+      end
+      command
+    end
+    
   end
   
 end
